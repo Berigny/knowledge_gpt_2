a54d72572c1011cc0f3842775ef79653481d8af3
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-25 18:31:38 +1000

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 from knowledge_gpt.ui import (
     wrap_doc_in_html,
     is_query_valid,
@@ -6,25 +7,21 @@ from knowledge_gpt.ui import (
     is_open_ai_key_valid,
     display_file_read_error,
 )
+
 from knowledge_gpt.core.caching import bootstrap_caching
+
 from knowledge_gpt.core.parsing import read_file
-from knowledge_gpt.core.chunking import chunk_file  # Assuming chunk_file function is available in this module
+from knowledge_gpt.core.chunking import chunk_file
 from knowledge_gpt.core.embedding import embed_files
 from knowledge_gpt.core.qa import query_folder
 from knowledge_gpt.core.utils import get_llm
 
-# New function to concatenate and chunk text from multiple files
-def chunk_multiple_files(files, chunk_size=300, chunk_overlap=0):
-    concatenated_text = ""
-    for file in files:
-        try:
-            concatenated_text += file.text + "\n"  # Adjusted line
-        except AttributeError as e:
-            st.error(f"Error extracting text from file: {e}")
-            continue  # skip to the next file on error
+# Initialize session state if it doesn't exist
+if 'processed' not in st.session_state:
+    st.session_state['processed'] = False
 
-    chunks = [concatenated_text[i:i + chunk_size] for i in range(0, len(concatenated_text), chunk_size - chunk_overlap)]
-    return chunks
+if 'queried' not in st.session_state:
+    st.session_state['queried'] = False
 
 EMBEDDING = "openai"
 VECTOR_STORE = "faiss"
@@ -33,69 +30,71 @@ MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 st.set_page_config(page_title="HCD-Helper", layout="wide")
 st.header("HCD-Helper")
 
+# Enable caching for expensive functions
 bootstrap_caching()
-
-model: str = st.selectbox("Model", options=MODEL_LIST)  # move this line up
 
 openai_api_key = st.text_input(
     "Enter your OpenAI API key. You can get a key at "
     "[https://platform.openai.com/account/api-keys](https://platform.openai.com/account/api-keys)"
 )
 
-if not is_open_ai_key_valid(openai_api_key, model):
-    st.stop()
+# Check the session state to decide whether to show the file uploader or not
+if not st.session_state['processed'] or st.session_state['queried']:
+    uploaded_file = st.file_uploader(
+        "Upload a pdf, docx, or txt file",
+        type=["pdf", "docx", "txt"],
+        help="Scanned documents are not supported yet!",
+    )
+else:
+    st.warning("Please query the processed document before uploading a new one.")
+    uploaded_file = None  # Set to None to prevent further processing
 
-uploaded_files = st.file_uploader(
-    "Upload pdf, docx, or txt files",
-    type=["pdf", "docx", "txt"],
-    accept_multiple_files=True,
-    help="Scanned documents are not supported yet!",
-)
+model: str = st.selectbox("Model", options=MODEL_LIST)  # type: ignore
 
 with st.expander("Advanced Options"):
     return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
     show_full_doc = st.checkbox("Show parsed contents of the document")
 
-if not uploaded_files:
+if not uploaded_file:
     st.stop()
-
-files = []
-for uploaded_file in uploaded_files:
-    try:
-        file = read_file(uploaded_file)
-        files.append(file)
-    except Exception as e:
-        display_file_read_error(e, file_name=uploaded_file.name)
-        continue  # skip to the next file on error
 
 try:
-    chunked_corpus = chunk_multiple_files(files, chunk_size=300, chunk_overlap=0)
+    file = read_file(uploaded_file)
 except Exception as e:
-    st.error(f"Error chunking files: {e}")
+    display_file_read_error(e, file_name=uploaded_file.name)
+
+chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
+
+if not is_file_valid(file):
     st.stop()
 
-if not is_file_valid(files):
-    st.error("One or more files are invalid")
+if not is_open_ai_key_valid(openai_api_key, model):
     st.stop()
 
 with st.spinner("Indexing document... This may take a while⏳"):
     folder_index = embed_files(
-        files=chunked_corpus,
+        files=[chunked_file],
         embedding=EMBEDDING if model != "debug" else "debug",
         vector_store=VECTOR_STORE if model != "debug" else "debug",
         openai_api_key=openai_api_key,
     )
+
+# Set processed to True once the document is processed
+st.session_state['processed'] = True
 
 with st.form(key="qa_form"):
     query = st.text_area("Ask a question about the document")
     submit = st.form_submit_button("Submit")
 
 if show_full_doc:
-    concatenated_corpus = "\n".join([file.text for file in files])  # Adjusted line
     with st.expander("Document"):
-        st.markdown(f"<p>{wrap_doc_in_html(concatenated_corpus)}</p>", unsafe_allow_html=True)
+        # Hack to get around st.markdown rendering LaTeX
+        st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>", unsafe_allow_html=True)
 
 if submit:
+    # Set queried to True once the document is queried
+    st.session_state['queried'] = True
+
     if not is_query_valid(query):
         st.stop()
 
