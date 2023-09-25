4aca70ade6253e8ed39371d4e0844c62929c857b
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-25 12:07:29 +1000

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from knowledge_gpt.ui import (
     wrap_doc_in_html,
     is_query_valid,
@@ -7,66 +6,78 @@ from knowledge_gpt.ui import (
     is_open_ai_key_valid,
     display_file_read_error,
 )
-
 from knowledge_gpt.core.caching import bootstrap_caching
-
 from knowledge_gpt.core.parsing import read_file
-from knowledge_gpt.core.chunking import chunk_file
+from knowledge_gpt.core.chunking import chunk_file  # Assuming chunk_file function is available in this module
 from knowledge_gpt.core.embedding import embed_files
 from knowledge_gpt.core.qa import query_folder
 from knowledge_gpt.core.utils import get_llm
+
+# New function to concatenate and chunk text from multiple files
+def chunk_multiple_files(files, chunk_size=300, chunk_overlap=0):
+    concatenated_text = "\n".join([file.get_text() for file in files])  # Assuming DocxFile has a get_text() method
+    chunks = [concatenated_text[i:i + chunk_size] for i in range(0, len(concatenated_text), chunk_size - chunk_overlap)]
+    return chunks
+
 
 EMBEDDING = "openai"
 VECTOR_STORE = "faiss"
 MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 
-# Uncomment to enable debug mode
-# MODEL_LIST.insert(0, "debug")
-
 st.set_page_config(page_title="HCD-Helper", layout="wide")
 st.header("HCD-Helper")
 
-# Enable caching for expensive functions
 bootstrap_caching()
-
-# Removed the sidebar() function call
 
 openai_api_key = st.text_input(
     "Enter your OpenAI API key. You can get a key at "
     "[https://platform.openai.com/account/api-keys](https://platform.openai.com/account/api-keys)"
 )
 
-uploaded_file = st.file_uploader(
-    "Upload a pdf, docx, or txt file",
+uploaded_files = st.file_uploader(
+    "Upload pdf, docx, or txt files",
     type=["pdf", "docx", "txt"],
+    accept_multiple_files=True,
     help="Scanned documents are not supported yet!",
 )
 
-model: str = st.selectbox("Model", options=MODEL_LIST)  # type: ignore
+if not is_open_ai_key_valid(openai_api_key, model):
+    st.stop()
+
+
+model: str = st.selectbox("Model", options=MODEL_LIST)
 
 with st.expander("Advanced Options"):
     return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
     show_full_doc = st.checkbox("Show parsed contents of the document")
 
-if not uploaded_file:
+if not uploaded_files:
     st.stop()
 
-try:
-    file = read_file(uploaded_file)
-except Exception as e:
-    display_file_read_error(e, file_name=uploaded_file.name)
+files = []
+for uploaded_file in uploaded_files:
+    try:
+        file = read_file(uploaded_file)
+        files.append(file)
+    except Exception as e:
+        display_file_read_error(e, file_name=uploaded_file.name)
+        continue  # skip to the next file on error
 
-chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
 
-if not is_file_valid(file):
+
+# Validate the concatenated corpus before proceeding
+concatenated_corpus = "\n".join([file.get_text() for file in files])
+
+# Add this line to get chunked_corpus
+chunked_corpus = chunk_multiple_files(files, chunk_size=300, chunk_overlap=0)
+
+if not is_file_valid(concatenated_corpus):
     st.stop()
 
-if not is_open_ai_key_valid(openai_api_key, model):
-    st.stop()
 
 with st.spinner("Indexing document... This may take a while⏳"):
     folder_index = embed_files(
-        files=[chunked_file],
+        files=chunked_corpus,
         embedding=EMBEDDING if model != "debug" else "debug",
         vector_store=VECTOR_STORE if model != "debug" else "debug",
         openai_api_key=openai_api_key,
@@ -78,8 +89,7 @@ with st.form(key="qa_form"):
 
 if show_full_doc:
     with st.expander("Document"):
-        # Hack to get around st.markdown rendering LaTeX
-        st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>", unsafe_allow_html=True)
+        st.markdown(f"<p>{wrap_doc_in_html(concatenated_corpus)}</p>", unsafe_allow_html=True)
 
 if submit:
     if not is_query_valid(query):
