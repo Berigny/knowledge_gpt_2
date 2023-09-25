64fe9210b4c320ad9efae981dbfb5ce024acf5a5
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-25 11:29:27 +1000

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-from knowledge_gpt.components.sidebar import sidebar
-
 from knowledge_gpt.ui import (
     wrap_doc_in_html,
     is_query_valid,
@@ -18,7 +16,6 @@ from knowledge_gpt.core.embedding import embed_files
 from knowledge_gpt.core.qa import query_folder
 from knowledge_gpt.core.utils import get_llm
 
-
 EMBEDDING = "openai"
 VECTOR_STORE = "faiss"
 MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
@@ -26,23 +23,18 @@ MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
 
-st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
-st.header("📖KnowledgeGPT")
+st.set_page_config(page_title="HCD-Helper", layout="wide")
+st.header("HCD-Helper")
 
 # Enable caching for expensive functions
 bootstrap_caching()
 
-sidebar()
+# Removed the sidebar() function call
 
-openai_api_key = st.session_state.get("OPENAI_API_KEY")
-
-
-if not openai_api_key:
-    st.warning(
-        "Enter your OpenAI API key in the sidebar. You can get a key at"
-        " https://platform.openai.com/account/api-keys."
-    )
-
+openai_api_key = st.text_input(
+    "Enter your OpenAI API key. You can get a key at "
+    "[https://platform.openai.com/account/api-keys](https://platform.openai.com/account/api-keys)"
+)
 
 uploaded_file = st.file_uploader(
     "Upload a pdf, docx, or txt file",
@@ -55,7 +47,6 @@ model: str = st.selectbox("Model", options=MODEL_LIST)  # type: ignore
 with st.expander("Advanced Options"):
     return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
     show_full_doc = st.checkbox("Show parsed contents of the document")
-
 
 if not uploaded_file:
     st.stop()
@@ -70,10 +61,8 @@ chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
 if not is_file_valid(file):
     st.stop()
 
-
 if not is_open_ai_key_valid(openai_api_key, model):
     st.stop()
-
 
 with st.spinner("Indexing document... This may take a while⏳"):
     folder_index = embed_files(
@@ -87,12 +76,10 @@ with st.form(key="qa_form"):
     query = st.text_area("Ask a question about the document")
     submit = st.form_submit_button("Submit")
 
-
 if show_full_doc:
     with st.expander("Document"):
         # Hack to get around st.markdown rendering LaTeX
         st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>", unsafe_allow_html=True)
-
 
 if submit:
     if not is_query_valid(query):
