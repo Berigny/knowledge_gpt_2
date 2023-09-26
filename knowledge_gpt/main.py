3d943331c29a7c5ad1f2d0c00b309f6d27ccb00e
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-26 14:54:39 +1000

    Update main.py

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from knowledge_gpt.ui import (
     wrap_doc_in_html,
     is_query_valid,
@@ -7,9 +6,7 @@ from knowledge_gpt.ui import (
     is_open_ai_key_valid,
     display_file_read_error,
 )
-
 from knowledge_gpt.core.caching import bootstrap_caching
-
 from knowledge_gpt.core.parsing import read_file
 from knowledge_gpt.core.chunking import chunk_file
 from knowledge_gpt.core.embedding import embed_files
@@ -22,6 +19,9 @@ if 'processed' not in st.session_state:
 
 if 'queried' not in st.session_state:
     st.session_state['queried'] = False
+
+# Initialize an empty string to accumulate document text
+all_documents_text = ""
 
 EMBEDDING = "openai"
 VECTOR_STORE = "faiss"
@@ -72,6 +72,9 @@ for uploaded_file in uploaded_files:
     chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
     processed_files.append(chunked_file)  # Store processed files for later access
 
+    # Accumulate document text
+    all_documents_text += '\n'.join([doc.text for doc in chunked_file.docs]) + '\n'
+
     with st.spinner("Indexing document... This may take a while⏳"):
         folder_index = embed_files(
             files=[chunked_file],
@@ -81,74 +84,5 @@ for uploaded_file in uploaded_files:
         )
         folder_indices.append(folder_index)  # Store folder indices for later querying
 
-# Create a combined document and embed it
-combined_document = '\n'.join([doc.text for file in processed_files for doc in file.docs])
-
-with st.spinner("Indexing combined document... This may take a while⏳"):
-    combined_folder_index = embed_files(
-        files=[chunk_file(combined_document, chunk_size=300, chunk_overlap=0)],
-        embedding=EMBEDDING if model != "debug" else "debug",
-        vector_store=VECTOR_STORE if model != "debug" else "debug",
-        openai_api_key=openai_api_key,
-    )
-
 st.session_state['processed'] = True  # Set processed to True once documents are processed
 
-if show_full_doc:
-    with st.expander("All Documents"):
-        st.markdown(f"<p>{wrap_doc_in_html(combined_document)}</p>", unsafe_allow_html=True)
-
-with st.form(key="qa_form1"):
-    query = st.text_area("Ask a question about the document")
-    submit = st.form_submit_button("Submit")
-
-document_options = ["All documents"] + [f"Document {i}" for i, _ in enumerate(uploaded_files, start=1)]
-selected_document = st.selectbox("Select document", options=document_options)
-
-if submit:
-    if not is_query_valid(query):
-        st.stop()
-
-    # Output Columns
-    answer_col, sources_col = st.columns(2)
-
-    llm = get_llm(model=model, openai_api_key=openai_api_key, temperature=0)
-
-    if selected_document == "All documents":
-        result = query_folder(
-            folder_index=combined_folder_index,
-            query=query,
-            return_all=return_all_chunks,
-            llm=llm,
-        )
-        with answer_col:
-            st.markdown("#### Answer")
-            st.markdown(result.answer)
-
-        with sources_col:
-            st.markdown("#### Sources")
-            for source in result.sources:
-                st.markdown(source.page_content)
-                st.markdown(source.metadata["source"])
-                st.markdown("---")
-    else:
-        folder_index = folder_indices[document_options.index(selected_document) - 1]  # Adjusted index due to "All documents" option
-        result = query_folder(
-            folder_index=folder_index,
-            query=query,
-            return_all=return_all_chunks,
-            llm=llm,
-        )
-        with answer_col:
-            st.markdown("#### Answer")
-            st.markdown(result.answer)
-
-        with sources_col:
-            st.markdown("#### Sources")
-            for source in result.sources:
-                st.markdown(source.page_content)
-                st.markdown(source.metadata["source"])
-                st.markdown("---")
-
-    # Set queried to True after processing a query
-    st.session_state['queried'] = True
